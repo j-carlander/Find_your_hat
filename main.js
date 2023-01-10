a64c50aae5cdecfe9c53b35db365adcddec29d92
j-carlander <johannes.carlander@gmail.com>
2023-01-10 14:48:27 +0100

    Add class Field and method print()

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ const hat = "^";
 const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
+
+class Field {
+  constructor(fieldArray) {
+    this.fieldArray = fieldArray;
+  }
+
+  print() {
+    this.fieldArray.forEach((row) => {
+      console.log(row.join(""));
+    });
+  }
+}
+
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+
+myField.print();
